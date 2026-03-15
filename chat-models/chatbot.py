64dbb14a81997f950a2c33d8e85c6039832292ff
mk sinha <mksinha77756@gmail.com>
2026-03-15 17:36:58 +0530

    messages

--- chat-models/chatbot.py
+++ chat-models/chatbot.py
@@ -5,13 +5,16 @@ from langchain.chat_models import init_chat_model
 # model = init_chat_model("gpt-4.1")
 # response = model.invoke("what is youer ip address?")
 # print(response.content)
-
+messages =[]
 from langchain_openai import ChatOpenAI
 model = ChatOpenAI(model="gpt-4.1",temperature=0.5,max_tokens=20)
+print("______________________________welcome type 0 to exit ______________________________")
 while True :
-    print("______________________________welcome type 0 to exit ______________________________")
+
     promt = input("chat now : ")
+    messages.append(promt)
     if promt== "0":
         break
-    response = model.invoke(promt)
+    response = model.invoke(messages)
+    messages.append(response.content)
     print("bot : ",response.content)
